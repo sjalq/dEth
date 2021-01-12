32b126c64729e3dd738aba256692a99a626591b1
sjalq <schalk.dormehl@gmail.com>
2021-01-12 17:44:08 +0200

    Added rewards to gulper for poking

--- smart-contracts/dETH/contracts/Gulper.sol
+++ smart-contracts/dETH/contracts/Gulper.sol
@@ -1,18 +1,16 @@
 pragma solidity ^0.5.0;
 
-contract IERC20
-{
-    function balanceOf(address _account) public returns(uint);
-    function totalSupply() public returns(uint);
-}
+import "../../common.5/openzeppelin/math/SafeMath.sol";
+import "../../common.5/openzeppelin/token/ERC20/IERC20.sol";
 
 contract IERC20Wrapper is IERC20
 {
-    function deposit(uint _amount) public;
+    function deposit() payable public;
+    function withdraw(uint _amount) public;
 }
 
 contract BalancerPool is IERC20 
-{
+{   
     function calcPoolOutGivenSingleIn(
             uint tokenBalanceIn,
             uint tokenWeightIn,
@@ -34,13 +32,39 @@ contract Gulper
 {
     // goal: a contract to receive funds in the form of eth and erc20s and spend them in predetermined ways
 
+    using SafeMath for uint256;
+
     constructor () public { }
 
-    address public constant WETH;
-    address public constant POOL;
+    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant POOL = 0x5277a42ef95ECa7637fFa9E69B65A12A089FE12b;
 
-    event permafrostGulped(uint _amount);
-    function gulpEthToPermafrost() 
+    event gulped(uint _ether, uint _poolTokens, uint _pokeReward);
+
+    function getGulpDetails()
+        public
+        view
+        returns (uint _wethBalanceToConvert, uint _minTokensToClaim, uint _pokeReward)
+    {
+        uint ethBalance = address(this).balance;
+        uint wethBalance = ethBalance.add(IERC20Wrapper(WETH).balanceOf(address(this)));
+        // following line pays out 0.1% of the weth to the person poking this contract
+        _pokeReward = wethBalance.div(1000);
+        _wethBalanceToConvert = wethBalance.sub(_pokeReward);
+        uint wethPoolBalance = IERC20Wrapper(WETH).balanceOf(POOL);
+        uint poolBalance = BalancerPool(POOL).totalSupply();
+        uint tokensOut = BalancerPool(POOL)
+            .calcPoolOutGivenSingleIn(
+                wethPoolBalance,
+                5 * 10**18,
+                poolBalance,
+                10 * 10**18,
+                _wethBalanceToConvert,
+                10**17);
+        _minTokensToClaim = tokensOut.mul(100 * 10**9).div(95 * 10**9);
+    }
+
+    function gulp() 
         public
     {
         // goals: 
@@ -52,22 +76,17 @@ contract Gulper
         // *calculate the min amount of pool tokens that we should receive for that much eth
         // *call joinswapExternAmountIn() for that amount of weth
         // *send the pool tokens to 0x01
+        // *send a reward to msg.sender for poking the contract. 
 
-        uint ethBalance = address(this).balance;
-        IERC20Wrapper(WETH).deposit(ethBalance);
-        uint wethPoolBalance = IERC20Wrapper(WETH).balanceOf(POOL);
-        uint poolBalance = BalancerPool(POOL).totalSupply();
-        uint minTokensToClaim = BalancerPool(POOL)
-            .calcPoolOutGivenSingleIn(
-                wethPoolBalance,
-                5*10**18,
-                poolBalance,
-                10*10**18,
-                ethBalance,
-                10*17) * (100 * 10**9 / 95 * 10**9);
-
-        BalancerPool(POOL).joinswapExternAmountIn(WETH, ethBalance, minTokensToClaim);
+        (uint wethBalanceToConvert, uint minTokensToClaim, uint pokeReward) = getGulpDetails();
+        
+        IERC20Wrapper(WETH).deposit.value(address(this).balance)();
+        BalancerPool(POOL).joinswapExternAmountIn(WETH, wethBalanceToConvert, minTokensToClaim);
         uint poolTokensToBurn = BalancerPool(POOL).balanceOf(address(this)); 
         BalancerPool(POOL).transfer(address(1), poolTokensToBurn);
+
+        IERC20Wrapper(WETH).withdraw(pokeReward);
+        emit gulped(wethBalanceToConvert, poolTokensToBurn, pokeReward);
+        msg.sender.call.value(pokeReward)("");
     }
 }
