587aa0ba79a0e9c198466d62c26acbb60cfd67cd
Mark Virchenko <mark.virchenko@redduck.io>
2021-05-20 15:07:59 +0300

    tidy up program.fs

--- smart-contracts/dETH2/tests/Program.fs
+++ smart-contracts/dETH2/tests/Program.fs
@@ -1,12 +1,7 @@
 ﻿module Program
 
-open dEthTestsBase
-
 [<EntryPoint>]
 let main _ =
-    //let s = dEthTests.``dEth - automate - check that an authorised address can change the automation settings`` "owner" 180 220 220 1 1 1
-    //let s = dEthTests.``dEth - automate - check that an authorised address can change the automation settings`` "contract" 180 220 220 1 1 1
-    let s = dEthTests.``dEth - automate - check that an authorised address can change the automation settings`` foundryTreasury 180 220 220 1 1 1
-    let id = TestBase.ethConn.MakeSnapshot ()
+    TestBase.ethConn.MakeSnapshot () |> ignore
     TestBase.ethConn.RestoreSnapshot ()
     0
